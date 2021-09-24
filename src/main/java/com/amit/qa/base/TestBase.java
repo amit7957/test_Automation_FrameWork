912020bf5f413be7f2577c03e48b8f90936c8ea6
ajain <ajain@tree.com>
2021-09-24 12:26:01 +0530

    aaj ka hai

--- src/main/java/com/amit/qa/base/TestBase.java
+++ src/main/java/com/amit/qa/base/TestBase.java
@@ -4,18 +4,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import com.amit.qa.util.TestUtil;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 	
 	public static WebDriver driver;
 	public static Properties prop;
-	static String driverPath =  "./src/main/resources/Driver/";
 	static String extension = ".exe";
 	
 	public TestBase(){
@@ -33,16 +36,13 @@ public class TestBase {
 	
 	
 	public static void initialization() throws Exception{
-		String browserName = prop.getProperty("browser");
-		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", new File(driverPath + "chromedriver" + extension).getCanonicalPath());	
-			driver = new ChromeDriver(); 
-		}
-		else {
-			System.out.println("No Browser Set");
-		}
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
+	    String browserName = prop.getProperty("browser");
+	    if(browserName.equals("chrome"))
+	    WebDriverManager.chromedriver().setup();
+        else {
+            System.out.println("No Browser Set");
+        }
+	    driver=new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
